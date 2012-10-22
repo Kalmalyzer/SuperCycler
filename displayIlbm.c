435781d9aa20b8d104629101c4957f0c064e7fb2
Mikael Kalms <mikael@kalms.org>
2012-10-22 23:25:31 +0200

    Interactive speed and blend controls
    Palette is either cycled hard (non blended) or through linear interpolation (blended).
    Use keys 1-9 to control speed, space to toggle pause/unpause, 'b' to toggle blend on/off.
    
    pollMessages() has been rewritten to not discard events if multiple arrive during the same frame, too.

--- displayIlbm.c
+++ displayIlbm.c
@@ -90,7 +90,7 @@ void setPalette(uint numColors, uint32_t* colors)
 
  	LoadRGB32(&OSScreen->ViewPort, (ULONG*) palette);
 }
-void animatePalette(Ilbm* ilbm, uint frame)
+void animatePalette(Ilbm* ilbm, uint frame, bool blend)
 {
 	static uint32_t colors[256];
 
@@ -100,13 +100,41 @@ void animatePalette(Ilbm* ilbm, uint frame)
 	{
 		IlbmColorRange* range = &ilbm->colorRanges[rangeId];
 		uint colorsInRange = range->high - range->low + 1;
-		uint scaledRate = (frame * range->rate) / (16384*50);
-		uint offset = scaledRate % colorsInRange;
+		//uint scaledRate = (frame * range->rate) / (16384*50);
+		uint scaledFrame = frame;
+		
+		uint offset = (scaledFrame >> 16) % colorsInRange;
 		if (!range->reverse)
 			offset = (colorsInRange - offset) % colorsInRange;
 		
-		for (uint colorId = 0; colorId < colorsInRange; ++colorId)
-			colors[range->low + colorId] = ilbm->palette.colors[range->low + (colorId + offset) % colorsInRange];
+		if (blend)
+		{
+			uint offsetFrac = (scaledFrame >> 8) & 0xff;
+			uint color1Weight = (scaledFrame >> 8) & 0xff;
+			uint color0Weight = 0x100 - color1Weight;
+			for (uint colorId = 0; colorId < colorsInRange; ++colorId)
+			{
+				uint color0 = (colorId + offset) % colorsInRange;
+				uint color1 = (color0 + colorsInRange - 1) % colorsInRange;
+
+				uint32_t rgb0 = ilbm->palette.colors[range->low + color0];
+				uint32_t rgb1 = ilbm->palette.colors[range->low + color1];
+
+				uint32_t rb0 = (rgb0 & 0x00ff00ff);
+				uint32_t rb1 = (rgb1 & 0x00ff00ff);
+				uint32_t rb = (((rb0 * color0Weight) + (rb1 * color1Weight)) >> 8) & 0x00ff00ff;
+				
+				uint32_t g0 = (rgb0 & 0x0000ff00);
+				uint32_t g1 = (rgb1 & 0x0000ff00);
+				uint32_t g = (((g0 * color0Weight) + (g1 * color1Weight)) >> 8) & 0x0000ff00;
+				
+				uint32_t rgb = rb | g;
+				colors[range->low + colorId] = rgb;
+			}
+		}
+		else
+			for (uint colorId = 0; colorId < colorsInRange; ++colorId)
+				colors[range->low + colorId] = ilbm->palette.colors[range->low + (colorId + offset) % colorsInRange];
 	}
 	
 	setPalette(ilbm->palette.numColors, colors);
@@ -116,10 +144,21 @@ typedef enum
 {
 	InputEvent_None,
 	InputEvent_Exit,
+	InputEvent_TogglePause,
+	InputEvent_Speed1,
+	InputEvent_Speed2,
+	InputEvent_Speed3,
+	InputEvent_Speed4,
+	InputEvent_Speed5,
+	InputEvent_Speed6,
+	InputEvent_Speed7,
+	InputEvent_Speed8,
+	InputEvent_Speed9,
+	InputEvent_ToggleBlend,
 } InputEvent;
 	
 
-InputEvent pollMessages(void)
+InputEvent getInputEvent(void)
 {
 	InputEvent event = InputEvent_None;
 
@@ -136,10 +175,19 @@ InputEvent pollMessages(void)
 
 				if (key == 27)
 					event = InputEvent_Exit;
+				else if (key >= '1' && key <= '9')
+					event = InputEvent_Speed1 + (key - '1');
+				else if (key == ' ')
+					event = InputEvent_TogglePause;
+				else if (key == 'b' || key == 'B')
+					event = InputEvent_ToggleBlend;
 			}
 		}
 
 		ReplyMsg((struct Message*) msg);
+		
+		if (event != InputEvent_None)
+			break;
 	}
 	
 	return event;
@@ -149,15 +197,32 @@ void displayLoop(Ilbm* ilbm)
 {
 	static int frame = 0;
 	bool exitFlag = false;
+	bool pause = false;
+	bool blend = false;
+	int speed = 5;
 	while (!exitFlag)
 	{
-		WaitBOVP(&OSScreen->ViewPort);
-		if (pollMessages() != InputEvent_None)
-			exitFlag = true;
+		WaitTOF();
+		//WaitBOVP(&OSScreen->ViewPort);
+		InputEvent event;
+		while ((event = getInputEvent()) != InputEvent_None)
+		{
+			if (event == InputEvent_Exit)
+				exitFlag = true;
+			else if (event == InputEvent_TogglePause)
+				pause = !pause;
+			else if (event == InputEvent_ToggleBlend)
+				blend = !blend;
+			else if (event >= InputEvent_Speed1 && event <= InputEvent_Speed9)
+			{
+				speed = (event - InputEvent_Speed1 + 1);
+			}
+		}
 
-		animatePalette(ilbm, frame);
-		
-		frame++;
+		animatePalette(ilbm, frame, blend);
+
+		if (!pause)
+			frame += ((65536 * (10 - speed)) / 20);
 	}
 }
 
@@ -202,6 +267,7 @@ void displayImage(Ilbm* ilbm)
 		SA_Title, "SuperCycler",
 		SA_ShowTitle, FALSE,
 		SA_Quiet, TRUE,
+		SA_Exclusive, TRUE,
 		SA_DisplayID, modeID,
 		TAG_DONE)))
 	{
